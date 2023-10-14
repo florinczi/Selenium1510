573d05cf00c6723ae971e92b937556a8a8060930
florinczi <140913385+florinczi@users.noreply.github.com>
2023-10-15 00:37:56 +0200

    Create constants.py

---
+++ SauceDemoBot/data/constants.py
@@ -0,0 +1,12 @@
+FRONTPAGE_URL = "https://www.saucedemo.com"
+
+USER = "standard_user"
+# Valid users: 
+# standard_user
+# locked_out_user
+# problem_user
+# performance_glitch_user
+# error_user
+
+PASSWORD = "secret_sauce"
+INVENTORY_URL = "https://www.saucedemo.com/inventory.html"
